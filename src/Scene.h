cb6752c40e4ea6ed702a81bbecd5097d42863afa
TroutFario <matis.begot@gmail.com>
2025-11-10 14:54:00 +0100

    branche spéciale !!!

--- src/Scene.h
+++ src/Scene.h
@@ -163,19 +163,31 @@ public:
             light.radius = 2.5f;
             light.powerCorrection = 2.f;
             light.type = LightType_Spherical;
-            light.material = Vec3(1, 1, 1);
+            light.material = Vec3(1);
             light.isInCamSpace = false;
         }
         {
+            // first sphere
             spheres.resize(spheres.size() + 1);
             Sphere &s = spheres[spheres.size() - 1];
             s.m_center = Vec3(1., 0., 0.);
             s.m_radius = 1.f;
             s.build_arrays();
             s.material.type = Material_Mirror;
-            s.material.diffuse_material = Vec3(1., 1., 1);
-            s.material.specular_material = Vec3(0.2, 0.2, 0.2);
+            s.material.diffuse_material = Vec3(1., 0., 0.);
+            s.material.specular_material = Vec3(0.2);
             s.material.shininess = 20;
+
+            // second sphere
+            spheres.resize(spheres.size() + 1);
+            Sphere &s2 = spheres[spheres.size() - 1];
+            s2.m_center = Vec3(-1., 0., 0.);
+            s2.m_radius = 1.f;
+            s2.build_arrays();
+            s2.material.type = Material_Mirror;
+            s2.material.diffuse_material = Vec3(0., 1., 0.);
+            s2.material.specular_material = Vec3(0.2);
+            s2.material.shininess = 20;
         }
     }
 
